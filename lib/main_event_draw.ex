5d4cf08935efb12a97f50803efea9060327c3acb
Derek Houck <derek@derekhouck.com>
2021-08-25 11:41:14 -0700

    Add create_starter_deck method

--- lib/main_event_draw.ex
+++ lib/main_event_draw.ex
@@ -1,10 +1,16 @@
 defmodule MainEventDraw do
-  def start_game do
-    IO.puts("Welcome to MAIN EVENT DRAAAAAAW!")
-    start_match()
+  def create_starter_deck do
+    Enum.map(0..4, fn x -> "Add 1 influence" end)
   end
 
-  def start_match do
+  def start_game do
+    IO.puts("Welcome to MAIN EVENT DRAAAAAAW!")
+    deck = create_starter_deck()
+    start_match(deck)
+  end
+
+  def start_match(deck) do
     IO.puts("Our first match is Katana Jazz vs Ruby Dynamite")
+    IO.puts("Starting deck: #{Enum.join(deck, ", ")}")
   end
 end
