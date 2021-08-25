9fb8caf72134bd1c71007d5b8a23cc4fa6040dde
Derek Houck <derek@derekhouck.com>
2021-08-25 11:14:49 -0700

    Add start_match method

--- lib/main_event_draw.ex
+++ lib/main_event_draw.ex
@@ -1,5 +1,10 @@
 defmodule MainEventDraw do
   def start_game do
-    "Welcome to MAIN EVENT DRAAAAAAW!"
+    IO.puts("Welcome to MAIN EVENT DRAAAAAAW!")
+    start_match()
+  end
+
+  def start_match do
+    IO.puts("Our first match is Katana Jazz vs Ruby Dynamite")
   end
 end
