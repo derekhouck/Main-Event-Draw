f2bd30d138236a78c24345bd717991817466e4ec
Derek Houck <derek@derekhouck.com>
2021-08-26 10:37:29 -0700

    Play all cards in hand

--- lib/main_event_draw.ex
+++ lib/main_event_draw.ex
@@ -8,7 +8,7 @@ defmodule MainEventDraw do
   end
 
   def draw_card_and_add_to_hand(deck, hand) do
-    { card, deck } = draw_card(deck)
+    { [ card ], deck } = draw_card(deck)
     { deck, [ card | hand ] }
   end
 
@@ -30,16 +30,25 @@ defmodule MainEventDraw do
     Enum.join(cards, ", ")
   end
 
+  def play_card(state) when length(state.hand) == 0 do
+    nil
+  end
+
   def play_card(state) do
-    { card, hand } = Enum.split(state.hand, 1)
-    %{ state | confidence: state.confidence + 1, hand: hand, discard: [ card | state.discard ] }
+    { [ card ], hand } = Enum.split(state.hand, 1)
+    IO.puts("Playing #{card}")
+    state = %{ state | confidence: state.confidence + 1, hand: hand, discard: [ card | state.discard ] }
+    report_current_state(state)
+    play_card(state)
   end
 
   def report_current_state(state) do
+    IO.puts("---")
     IO.puts("Hand: #{join_cards(state.hand)}")
     IO.puts("Deck: #{Enum.count(state.deck)} cards") 
     IO.puts("Discard: #{Enum.count(state.discard)} cards")
     IO.puts("Confidence: #{state.confidence}")
+    IO.puts("---")
   end
 
   def start_game do
@@ -62,7 +71,6 @@ defmodule MainEventDraw do
       confidence: 0
     }
     report_current_state(state)
-    state = play_card(state)
-    report_current_state(state)
+    play_card(state)
   end
 end
