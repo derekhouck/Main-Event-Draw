68d5d85c800c5a44c678f56f4fc4d57ea6d27456
Derek Houck <derek@derekhouck.com>
2021-08-26 14:14:30 -0700

    Add function docs

--- lib/main_event_draw.ex
+++ lib/main_event_draw.ex
@@ -3,23 +3,37 @@ defmodule MainEventDraw do
     Provides methods for creating and running a game of Main Event Draw
   """
 
+  @doc """
+    Adds a card to the player's hand.
+  """
+  def add_card_to_hand(card, hand) do
+    [ card | hand ]
+  end
+
+  @doc """
+    Creates the starting deck of cards for the player.
+  """
   def create_starter_deck do
     Enum.map(0..9, fn _x -> "Add 1 to confidence" end)
   end
 
+  @doc """
+    Creates the gimmick deck of cards that can be acquired using `confidence`.
+  """
   def create_gimmick_deck do
     Enum.map(0..9, fn _x -> "Add 1 to excitement" end)
   end
 
+  @doc """
+    Draws a card from the deck. Returns a tuple containing two lists: the card drawn and the remainder of the deck.
+  """
   def draw_card(deck) do
     Enum.split(deck, 1)
   end
 
-  def draw_card_and_add_to_hand(deck, hand) do
-    { [ card ], deck } = draw_card(deck)
-    { deck, [ card | hand ] }
-  end
-
+  @doc """
+    Deals a hand of cards to the player from the deck.
+  """
   def deal_hand(deck, hand \\ [], cards_left_to_draw \\ 5)
 
   def deal_hand(deck, hand, cards_left_to_draw) when cards_left_to_draw == 0 do
@@ -27,14 +41,22 @@ defmodule MainEventDraw do
   end
 
   def deal_hand(deck, hand, cards_left_to_draw) do
-    { deck, hand } = draw_card_and_add_to_hand(deck, hand)
+    { [ card ], deck } = draw_card(deck)
+    hand = add_card_to_hand(card, hand)
+
     deal_hand(deck, hand, cards_left_to_draw - 1) 
   end
 
+  @doc """
+    Joins a list of cards together into a comma-separated string.
+  """
   def join_cards(cards) do
     Enum.join(cards, ", ")
   end
 
+  @doc """
+    Plays a card, removing it from the player's hand and increasing `confidence` by 1.
+  """
   def play_card(state) when length(state.hand) == 0 do
     start_turn(state)
   end
@@ -48,6 +70,9 @@ defmodule MainEventDraw do
     |> play_card
   end
 
+  @doc """
+    Puts the current state of the game in the console.
+  """
   def report_current_state(state) do
     IO.puts("---")
     IO.puts("Hand: #{join_cards(state.hand)}")
@@ -59,6 +84,9 @@ defmodule MainEventDraw do
     state
   end
 
+  @doc """
+    Starts a game of Main Event Draw from its inital state.
+  """
   def start_game do
     IO.puts("Welcome to MAIN EVENT DRAAAAAAW!")
 
@@ -72,12 +100,18 @@ defmodule MainEventDraw do
     |> start_match
   end
 
+  @doc """
+    Starts a new match.
+  """
   def start_match(state) do
     IO.puts("Our first match is Katana Jazz vs Ruby Dynamite")
 
     start_turn(state)
   end
 
+  @doc """
+    Starts a new turn. A new hand is dealt and the cards in the hand are played.
+  """
   def start_turn(state) when length(state.deck) == 0 do
     IO.puts("No more cards left to draw.")
   end
