b67c2deb019bc4a7e9e254f881556e3afe032d0b
Derek Houck <derek@derekhouck.com>
2021-08-26 11:35:15 -0700

    Add create_gimmick_deck method

--- lib/main_event_draw.ex
+++ lib/main_event_draw.ex
@@ -3,6 +3,10 @@ defmodule MainEventDraw do
     Enum.map(0..9, fn _x -> "Add 1 to confidence" end)
   end
 
+  def create_gimmick_deck do
+    Enum.map(0..9, fn _x -> "Add 1 to excitement" end)
+  end
+
   def draw_card(deck) do
     Enum.split(deck, 1)
   end
@@ -41,9 +45,10 @@ defmodule MainEventDraw do
   def play_card(state) do
     { [ card ], hand } = Enum.split(state.hand, 1)
     IO.puts("Playing #{card}")
-    state = %{ state | confidence: state.confidence + 1, hand: hand, discard: [ card | state.discard ] }
-    report_current_state(state)
-    play_card(state)
+
+    %{ state | confidence: state.confidence + 1, hand: hand, discard: [ card | state.discard ] }
+    |> report_current_state
+    |> play_card
   end
 
   def report_current_state(state) do
@@ -52,24 +57,26 @@ defmodule MainEventDraw do
     IO.puts("Deck: #{Enum.count(state.deck)} cards") 
     IO.puts("Discard: #{Enum.count(state.discard)} cards")
     IO.puts("Confidence: #{state.confidence}")
+    IO.puts("Gimmicks available: #{Enum.count(state.gimmicks)}")
     IO.puts("---")
+    state
   end
 
   def start_game do
     IO.puts("Welcome to MAIN EVENT DRAAAAAAW!")
-    deck = create_starter_deck()
-    start_match(deck)
-  end
 
-  def start_match(deck) do
-    IO.puts("Our first match is Katana Jazz vs Ruby Dynamite")
-
-    state = %{
-      deck: deck,
+    %{
+      deck: create_starter_deck(),
       discard: [],
+      gimmicks: create_gimmick_deck(),
       hand: [],
       confidence: 0
     }
+    |> start_match
+  end
+
+  def start_match(state) do
+    IO.puts("Our first match is Katana Jazz vs Ruby Dynamite")
 
     start_turn(state)
   end
@@ -80,8 +87,8 @@ defmodule MainEventDraw do
 
   def start_turn(state) do
     { deck, hand } = deal_hand(state.deck)
-    state = %{ state | confidence: 0, deck: deck, hand: hand }
-    report_current_state(state)
-    play_card(state)
+    %{ state | confidence: 0, deck: deck, hand: hand }
+    |> report_current_state
+    |> play_card
   end
 end
